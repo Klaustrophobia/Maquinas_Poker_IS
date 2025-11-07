25dff331d040e822b08512d08e5fe5d70f83687e
Sants-3 <yosuposas@gmail.com>
2025-11-07 09:11:38 -0600

    Cambios Proveedor Backend #1

---
+++ backend/src/app/api/Proveedor/[id]/route.ts
@@ -0,0 +1,27 @@
+import { NextRequest } from "next/server";
+import { ProveedorController } from "@/controllers/Proveedor.controller";
+
+const proveedorController = new ProveedorController();
+
+export async function GET(
+  req: NextRequest,
+  context: { params: Promise<{ id: string }> }
+) {
+  const { id } = await context.params;
+  const numId = Number(id);
+  return await proveedorController.obtenerProveedorPorId(numId);
+}
+
+export async function PUT(req: NextRequest, 
+  context: { params: Promise<{ id: string }> }
+) {
+  const { id } = await context.params;
+  const numId = Number(id);
+  return await proveedorController.actualizarDatosProveedor(numId, req);
+}
+
+export async function DELETE(req: NextRequest, context: { params: Promise<{ id: string }> }) {
+  const { id } = await context.params;
+  const numId = Number(id);
+  return await proveedorController.eliminarProveedor(numId);
+}
---
+++ backend/src/app/api/Proveedor/route.ts
@@ -0,0 +1,12 @@
+import { NextRequest } from "next/server";
+import { ProveedorController } from "@/controllers/Proveedor.controller";
+
+const proveedorController = new ProveedorController();
+
+export async function GET() {
+  return await proveedorController.obtenerTodosLosProveedores();
+}
+
+export async function POST(req: NextRequest) {
+  return await proveedorController.crearProveedor(req);
+}
