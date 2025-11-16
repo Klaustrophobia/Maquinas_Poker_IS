3306ccb0c5d501c426d577d4e2b926d80340c5e0
unknown <joseenrique.funez@gmail.com>
2025-11-15 20:45:18 -0600

    Arreglos Backend #6

--- backend/src/lib/db.ts
+++ backend/src/lib/db.ts
@@ -3,16 +3,17 @@ import { DataSource } from 'typeorm';
 import { Usuario } from "../entities/Usuario";
 import { Proveedor } from "../entities/Proveedor";
 import { Repuesto } from "../entities/Repuesto";
+import { Maquina } from '@/entities/Maquina';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USER || 'soporte',
-  password: process.env.DB_PASS || 'password',
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASS || 'oracle',
   database: process.env.DB_NAME || 'Maquinas_poker',
   synchronize: false,
-  entities: [Usuario, Proveedor, Repuesto],
+  entities: [Usuario, Proveedor, Repuesto, Maquina],
   logging: true,
 });
 
